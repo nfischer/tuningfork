16efc0b284586a6f67b7e8cf870924523d3670f4
Nate Fischer <ntfschr@yahoo.com>
2013-11-19 01:52:41 -0800

    Added a script for compiling the program. Added the tune bash script.

--- tuningFork.c
+++ tuningFork.c
@@ -17,18 +17,29 @@ int main()
 {
 	double freq, duration;
 	char fname[50]; // arbitrary file name limit
+
+	// get user input
 	printf("Input desired frequency (Hz): ");
 	scanf("%lf", &freq);
 	printf("Input desired duration (sec): ");
 	scanf("%lf", &duration);
 	printf("Enter desired file name: ");
-	scanf("%s", &fname);
+	scanf("%s", fname);
+
+	// DEBUG (check user input)
+	printf("You entered %lf Hz freq\n", freq);
+	printf("You entered %lf s duration\n", duration);
+	printf("You entered <%s> as your file name\n", fname);
+
+	// Generate the file
+	genFile(freq, time, fname);
 
 	return 0;
 }
 
 
 
+// I'll let you start on this function, Freitag
 
 void genFile(double freq, double time, char* fname)
 {
