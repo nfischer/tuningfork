53ddd4afa1dbdadd12f99fca1aa116f28fed66d8
freitagbr <freitagbr@gmail.com>
2014-03-26 00:42:21 -0700

    Update tuningFork.c with .wav file output, fixed a 1 second in length

--- tuningFork.c
+++ tuningFork.c
@@ -15,13 +15,26 @@ Started November 2013
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define PI (3.141592653589793)
+#define PI (3.14159265)
 
 /* Global constants */
 const double MIN_FREQ = 0;
 const double MAX_FREQ = 22050; /* 22050 Hz */
 const double MIN_DURATION = 0; 
 const double MAX_DURATION = 60 * 100; /* 100 minutes */
+const char* HEADER_ChunkID = "\x52\x49\x46\x46"; /* RIFF */
+const char* HEADER_Chunksize = "\xF6\xCC\x02\x00";
+const char* HEADER_Format = "\x57\x41\x56\x45"; /* WAVE */
+const char* HEADER_Subchunk1ID = "\x66\x6D\x74\x20"; /* fmt */
+const char* HEADER_SubchunkSize = "\x12\x00\x00\x00";
+const char* HEADER_AudioFormat = "\x01\x00";
+const char* HEADER_Channels = "\x02\x00";
+const char* HEADER_SampleRate = "\x44\xAC\x00\x00";
+const char* HEADER_ByteRate = "\x10\xB1\x02\x00";
+const char* HEADER_BlockAlign = "\x04\x00";
+const char* HEADER_BitsPerSample = "\x10\x00";
+const char* HEADER_Subchunk2ID = "\x64\x61\x74\x61"; /* Data */
+const char* HEADER_Subchunk2Size = "\x10\xB1\x02\x00"; /* 1 sec */
 
 
 void genFile(double freq, double time, char* fname);
@@ -97,54 +110,40 @@ int main()
 	return 0;
 }
 
-// I'll let you start on this function, Freitag
-
-typedef struct {
-		int16_t *sample; /* pointer to memory storing list of 16-bit signed samples */
-}PCM; 
-
-PCM *genSampleChunk(double duration)
-{
-	PCM *sampleList;
-
-	if(!(sampleList = (PCM*)malloc(sizeof(PCM)))) /* if malloc can't allocate enough memory, return NULL */
-	{
-		return NULL;
-	}
-
-	if(!(sampleList->sample = (int16_t*)malloc(sizeof(int16_t) * 44100 * duration))) /* if malloc can't allocate enough memory, return NULL */
-	{
-		return NULL;
-	}
-
-	return sampleList;
-}
-
-PCM *genSampleList(double freq, double duration)
-{
-	PCM *sampleList; /* pointer to list of 16-bit samples */
-	int i;			 /* counter for loop that generates consecutive 16-bit samples */
-
-	sampleList = genSampleChunk(duration);
-
-	for (i = 0; i < (44100 * duration); i++)
-	{
-		sampleList->sample[i] = sin(2 * PI * i / freq);
-	}
-
-	return sampleList;
-}
-
 void genFile(double freq, double duration, char* fname)
 {
-	int16_t sample = 44100;	//	Sample rate of audio (44.1 KHz)
+	FILE *OutFile;
+	int16_t sample; /* pointer to list of 16-bit samples */
+	int i;			 /* counter for loop that generates consecutive 16-bit samples */
+
+	duration = 1; /* Hard-coded to 1 second */
+
+	OutFile = fopen(fname, "a");
+	fputs(HEADER_ChunkID, OutFile);
+	fputs(HEADER_Chunksize, OutFile);
+	fputs(HEADER_Format, OutFile);
+	fputs(HEADER_Subchunk1ID, OutFile);
+	fputs(HEADER_SubchunkSize, OutFile);
+	fputs(HEADER_AudioFormat, OutFile);
+	fputs(HEADER_Channels, OutFile);
+	fputs(HEADER_SampleRate, OutFile);
+	fputs(HEADER_ByteRate, OutFile);
+	fputs(HEADER_BlockAlign, OutFile);
+	fputs(HEADER_BitsPerSample, OutFile);
+	fputs(HEADER_Subchunk2ID, OutFile);
+	fputs(HEADER_Subchunk2Size, OutFile);
+
+	if (OutFile != NULL)
+	{
+		for (i = 0; i < (44100 * duration); i++)
+		{
+			sample = (0x7FFF) * sin(2 * PI * (double) i / freq);
+			printf("%d\n", sample);
+			fputc(sample, OutFile); /* Left channel */
+			fputc(sample, OutFile); /* Right channel */
+
+		}
+	}
 	
-	return; // DEBUG
-}
-
-
-void genWave(char* fname, unsigned long numSamples, short int* data,int sampleSize)
-{
-
-	return;
+	fclose(OutFile);
 }
